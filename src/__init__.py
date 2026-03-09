b2608d86db660f833fb1e5de400ccb902de79004
Serega Ivkin <kroll-87@mail.ru>
2026-03-09 15:32:56 +0300

    &&&&

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from src.base_product import BaseProduct
+from src.mixins import ReprMixin
+from src.product import Product, Smartphone, LawnGrass
+from src.category import Category
+
+__all__ = [
+    'BaseProduct',
+    'ReprMixin',
+    'Product',
+    'Smartphone',
+    'LawnGrass',
+    'Category'
+]
