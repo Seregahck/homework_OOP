bfcab60280f3ba0ed9c33442189239c4a25864f0
Serega Ivkin <kroll-87@mail.ru>
2026-03-13 10:12:37 +0300

    add

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
-from src.repr_mixin import ReprMixin  # noqa: F401
-from src.product import Product  # noqa: F401
-from src.category import Category  # noqa: F401
+from src.repr_mixin import ReprMixin
+from src.product import Product
+from src.category import Category
 
 
 __all__ = ['ReprMixin', 'Product', 'Category']
